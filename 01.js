5167264e66ed3b39e6c7a74dce5ae86202381b8c
[C[B[A[A[A[A[D[C[C[C[C[C[C[C[C[C[CiLi01[D[D[C[C[C[B[D[D[D[D[D[D[D[D[D[D[Cilinca.pricop100@gmail.com[C[B[C[C[C[C[C[C[C <ilinca.pricop100@gmail.com>
2023-09-13 17:53:12 +0100

    drafting

--- 01.js
+++ 01.js
@@ -11,5 +11,5 @@ function displayImages() {
     index = 1;
   }
   images[index-1].style.display = "block";
-  setTimeout(displayImages, 3700); // Change image every 3.7 seconds
+  setTimeout(displayImages, 4000); // Change image every 3.7 seconds
 }
